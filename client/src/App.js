72fa36ca4e78d8bc966b3fb5049fee8bfd29f3d2
Luke-Berzins <lukeberzins16@gmail.com>
2021-04-27 15:55:58 -0400

    successfully posting requests to the database

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import { Switch, Route } from 'react-router-dom';
-
+import Login from './Login';
 const App = () => {
   return (
     <div className="center w85">
@@ -16,6 +16,8 @@ const App = () => {
             path="/create"
             component={CreateLink}
           />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/new" component={CreateLink} />
         </Switch>
       </div>
     </div>
